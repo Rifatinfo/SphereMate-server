9362a1732f1653e6826ccfe7c7ba0919afee5cc6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-10-31 18:47:04 +0600

    add update

--- index.js
+++ index.js
@@ -80,6 +80,23 @@ async function run() {
       res.send(result);
     })
 
+    // update a job in db 
+    app.put('/job/:id', async(req, res) =>{
+      const id = req.params.id;
+      const jobDate = req.body;
+      const query = {_id: new ObjectId(id)};
+      const option = {
+        upsert : true
+      }
+      const updateDoc = {
+        $set : {
+          ...jobDate,
+        }
+      }
+      const result = await jobsCollection.updateOne(query, updateDoc, option);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
